c0b272b3bb0ba05aeb6a4773b0916d7d3e4d517c
Ahmed Mahmoud <68530005+ahmed-mahmoud15@users.noreply.github.com>
2024-03-15 14:12:46 +0200

    Fix last element problem

--- InsertionSort.java
+++ InsertionSort.java
@@ -4,7 +4,7 @@ public class InsertionSort implements Sort {
     }
 
     public void sort(int[] arr, int low, int high) {
-        for (int i = low + 1; i < high; i++) {
+        for (int i = low + 1; i <= high; i++) {
             int key = arr[i];
             int j = i;
             while (j > 0 && arr[j - 1] > key) {
